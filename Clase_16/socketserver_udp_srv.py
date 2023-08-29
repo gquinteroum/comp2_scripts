ccdddd98f21bbe7bce6091c21172f3548c9925f1
Gabriel Quintero <gabriel.quintero@um.edu.ar>
2023-08-29 11:12:34 -0300

    Clase 16

---
+++ Clase_16/socketserver_udp_srv.py
@@ -0,0 +1,24 @@
+import socketserver
+import time
+
+class MyUDPHandler(socketserver.BaseRequestHandler):
+    """
+    This class works similar to the TCP handler class, except that
+    self.request consists of a pair of data and client socket, and since
+    there is no connection the client address must be given explicitly
+    when sending data back via sendto().
+    """
+
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print("{} wrote:".format(self.client_address[0]))
+        print(data)
+        socket.sendto(data.upper(), self.client_address)
+        time.sleep(20)
+        
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
+        server.serve_forever()
